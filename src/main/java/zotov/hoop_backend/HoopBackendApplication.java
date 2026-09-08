c85c331854a17372cfbd130c42a2d03fd32fd318
zookat <zotova.ea@gmail.com>
2026-09-08 18:57:25 +0200

    init: first commit

---
+++ src/main/java/zotov/hoop_backend/HoopBackendApplication.java
@@ -0,0 +1,13 @@
+package zotov.hoop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoopBackendApplication.class, args);
+	}
+
+}
