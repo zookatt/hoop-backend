9409e372a3e5f5a14b38130e9b3dbb7e869ab494
zookat <zotova.ea@gmail.com>
2026-09-11 18:53:54 +0200

    feat: add find by email method al user credential repo

--- src/main/java/zotov/hoop_backend/repository/UserCredentialsRepository.java
+++ src/main/java/zotov/hoop_backend/repository/UserCredentialsRepository.java
@@ -2,7 +2,8 @@ package zotov.hoop_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import zotov.hoop_backend.entity.UserCredentials;
+import java.util.Optional;
 
 public interface UserCredentialsRepository extends JpaRepository<UserCredentials, Integer> {
-
+    Optional<UserCredentials> findByEmail(String email);
 }
